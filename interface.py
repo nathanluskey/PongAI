42793d4b5aa1e7f49eee4f67a002d38dc7b95065
Nathan Luskey <34701768+nathanluskey@users.noreply.github.com>
2021-02-07 21:40:32 -0500

    Preparing RL files

---
+++ interface.py
@@ -0,0 +1,4 @@
+from agent import agent
+from pongEnviroment import pongEnviroment
+
+#TODO: Create table for utility of each state and action
--- pongEnviroment.py
+++ pongEnviroment.py
@@ -1,21 +1,34 @@
 from environment import Environment
 import numpy as np
 
+
 class pongEnviroment(Environment):
     def __init__(self, radiusOfShooting=1):
         # TODO: Create a local variable of the current cups layout and how many re-racks have been used
         # TODO: Incoroporate the radius of shooting to be how accurate someone is, roughly
-        self.currentState = None
+        self.currentState = np.array() #TODO: FIll THIS IN
     
     def getCurrentState(self):
         # TODO: write out a doc explaining this
         return self.currentState
 
-    def getPossibleActions(self, state):
+    def getPossibleActions(self):
         # TODO: Yea... this one is going to be fun... I think return position of each cup and if a re-rack can take place
-        pass
+        cupsLeft = self.currentState.sum()
+
+        if (cupsLeft == 2):
+            # TODO: Change this to push both cups to the back by default in self.currentState
+            pass
+        elif (cupsLeft == 3):
+            # TODO: This will be the 2(?) the ways 3 cups will be arranged
+            pass
+        elif (cupsLeft == 6):
+            # TODO: Figure this out...
+            pass
+
 
     def doAction(self, action):
+        # TODO: Check if action is a re-rack, and do this with updating the state
         # TODO: With some likelyhood hit a cup or surrounding cups, this is dependent on the radiusOfShooting
         pass
 
