8d703ee5b60af21429d2ce13c8b71d35f67b4c34
Nathan Luskey <34701768+nathanluskey@users.noreply.github.com>
2021-02-19 11:23:58 -0500

    Fixed minor errors

--- interface.py
+++ interface.py
@@ -1,29 +1,30 @@
 from agent import Agent
 from pongEnvironment import PongEnvironment
 
-class Game():
-    __innit__(self, qTable=dict()):
-        self.currentAgent = agent(qTable=)
+class Game:
+    def __init__(self, verbose=False, qTable=dict(), radiusOfShooting=0):
+        self.agent = Agent(qTable=qTable)
+        self.game = PongEnvironment(radiusOfShooting=radiusOfShooting)
+        self.verbose = verbose
 
+    def playGame(self):
+        while (not (self.game.isTerminal())):
+            currentState = self.game.getCurrentState()
+            actions = self.game.getPossibleActions()
+            actionPicked = self.agent.pickAction(currentState, actions)
+            reward = self.game.doAction(actionPicked)
+            if (self.verbose):
+                print("Reward: {}, Action Picked: \n{}".format(reward, actionPicked))
+            futureState = self.game.getCurrentState()
+            futureActions = self.game.getPossibleActions()
+            self.agent.updateQTable(currentState, actionPicked, reward, futureState, futureActions)
+    
+    def getQTable(self):
+        return self.agent.qTable
 
 
 if __name__ == "__main__":
-    universalQTable = dict()
-
-    currentAgent = agent(qTable=universalQTable)
-    game = pongEnviroment()
-
-    while (not (game.isTerminal())):
-        # Play a game
-        currentState = game.getCurrentState()
-        actions = game.getPossibleActions()
-        actionPicked = currentAgent.pickAction(currentState, actions)
-        reward = game.doAction(actionPicked)
-        print("Reward: {}, Action Picked: \n{}".format(reward, actionPicked))
-        futureState = game.getCurrentState()
-        futureActions = game.getPossibleActions()
-        currentAgent.updateQTable(currentState, actionPicked, reward, futureState, futureActions)
-    
-    universalQTable = currentAgent.qTable
-    print(universalQTable)
+    game = Game(verbose=True)
+    game.playGame()
+    print(game.getQTable())
     
