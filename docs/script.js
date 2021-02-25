59f4f4dde204abc47c459001bafa0f2f4105b6ee
Nathan Luskey <34701768+nathanluskey@users.noreply.github.com>
2021-02-25 15:52:41 -0500

    Working with javascript

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,21 @@
 // Global Variables
 var cupLayout = 10; 
+var ctx;
+var json;
 
 function main() {
     var c = document.getElementById("c");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle = "#FF0000";
+    ctx = c.getContext("2d");
+    ctx.fillStyle = "red";
     ctx.fillRect(0, 0, c.width, c.height);
+    console.log(qTable)
 }
 
 function dropMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-function selectCupLayout() {
+function selectCupLayout(newCupLayout) {
     console.log("Clicked Button\n");
+    cupLayout = newCupLayout;
 }
